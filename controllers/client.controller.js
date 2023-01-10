63648af3d51483287ef8f61d9395b60ff4f1186f
Samuel <30.3.samuel@hotmail.com>
2023-01-10 02:41:09 -0400

    editar datos completado

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -11,7 +11,7 @@ const crearNuevaLinea = (nombre, email,id) => {
     <td>
       <ul class="table__button-control">
         <li><a 
-          href="../screens/editar_cliente.html" 
+          href="../screens/editar_cliente.html?id=${id}" 
           class="simple-button simple-button--edit"
           >Editar</a
           >
--- service/client-service.js
+++ service/client-service.js
@@ -12,14 +12,34 @@ const listaClientes = () =>
   };
 
   const eliminarCliente = (id) => {
-    console.log("elimina a ----->",id)
     return fetch(`http://localhost:3000/perfil/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
   })
   }
+
+  const detalleCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) => 
+    respuesta.json()
+    )
+  }
+
+  const actualizarCliente = (nombre, email, id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({nombre, email})
+    })
+    .then( respuesta => respuesta)
+    .catch((err) => console.log(err))
+  }
+
 
   export const clientServices = {
     listaClientes,
     crearCliente,
-    eliminarCliente
+    eliminarCliente,
+    detalleCliente,
+    actualizarCliente,
   };
